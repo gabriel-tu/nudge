18c6209c04142774931e05d76952f24eacec6e8d
Gabriel Tu <gabriel8215@hotmail.com>
2025-02-05 13:28:31 -0800

    added screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ProfileScreen } from "./screens/ProfileScreen";
+import { WelcomeScreen } from "./screens/WelcomeScreen";
 
 export default function Index() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{ title: "Welcome wow" }}
+      />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+    </Stack.Navigator>
   );
 }
---
+++ app/screens/WelcomeScreen.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export const WelcomeScreen = ({ navigation }) => {
+  return <Text>welcome!</Text>;
+};
