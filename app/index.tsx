88e0bd8599fb34985b9dff00bff4ffc9c6d3d8bc
Gabriel Tu <gabriel8215@hotmail.com>
2025-02-06 14:16:22 -0800

    added react native paper

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,25 @@
 import { Link } from "expo-router";
 import { View } from "react-native";
+import { DefaultTheme, PaperProvider } from "react-native-paper";
 
 export default function Index() {
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: "#83CBC1",
+      secondary: "#849491",
+    },
+  };
+
   return (
-    <View>
-      home screen!
-      <Link href="/screens/profile">go to profile</Link>
-      {/* ...other links */}
-      <Link href="/screens/welcome">go to welcome</Link>
-    </View>
+    <PaperProvider>
+      <View>
+        home screen!
+        <Link href="/screens/profile">go to profile</Link>
+        {/* ...other links */}
+        <Link href="/screens/welcome">go to welcome</Link>
+      </View>
+    </PaperProvider>
   );
 }
