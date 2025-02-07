68828c040c3597bcb3542c5ce2f67be295541984
Gabriel Tu <gabriel8215@hotmail.com>
2025-02-06 17:21:43 -0800

    added login screen, new deps, and test file

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,14 @@
 import { Link } from "expo-router";
 import { View } from "react-native";
-import { DefaultTheme, PaperProvider } from "react-native-paper";
 
 export default function Index() {
-  const theme = {
-    ...DefaultTheme,
-    colors: {
-      ...DefaultTheme.colors,
-      primary: "#83CBC1",
-      secondary: "#849491",
-    },
-  };
-
   return (
-    <PaperProvider>
-      <View>
-        home screen!
-        <Link href="/screens/profile">go to profile</Link>
-        {/* ...other links */}
-        <Link href="/screens/welcome">go to welcome</Link>
-      </View>
-    </PaperProvider>
+    <View>
+      home screen!
+      <Link href="/screens/profile">go to profile</Link>
+      {/* ...other links */}
+      <Link href="/screens/welcome">go to welcome</Link>
+      <Link href="/screens/login">go to login</Link>
+    </View>
   );
 }
---
+++ app/screens/login.tsx
@@ -0,0 +1,83 @@
+import { Formik } from "formik";
+import { View, StyleSheet } from "react-native";
+import { Button, TextInput, Text } from "react-native-paper";
+import * as yup from "yup";
+
+const LoginScreen = () => {
+  const loginValidationSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Please enter a valid email")
+      .required("An email is required"),
+    password: yup
+      .string()
+      .min(6, ({ min }) => "Password must be at least ${min} characters")
+      .required("Please enter a password"),
+  });
+
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      onSubmit={(values) => console.log(values)} // TODO: replace with login logic
+    >
+      {({ handleChange, handleSubmit, values }) => (
+        <View style={styles.container}>
+          <Text variant="displayLarge" style={styles.title}>
+            Login
+          </Text>
+          <View style={styles.textFieldContainer}>
+            <TextInput
+              label="Email"
+              onChangeText={handleChange("email")}
+              value={values.email}
+              style={{ marginBottom: 16, width: 300 }}
+            />
+            <TextInput
+              label="Password"
+              onChangeText={handleChange("password")}
+              value={values.password}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button
+              mode="contained"
+              onPress={handleSubmit}
+              style={{ marginBottom: 16, width: 300 }}
+            >
+              Login
+            </Button>
+            <Button
+              mode="contained"
+              onPress={() => {
+                /* TODO: navigate to signup screen*/
+              }}
+            >
+              Signup
+            </Button>
+          </View>
+        </View>
+      )}
+    </Formik>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff", // remove?
+    paddingHorizontal: 20,
+  },
+  title: {
+    marginBottom: 24,
+  },
+  textFieldContainer: {
+    padding: 24,
+  },
+  buttonContainer: {
+    padding: 24,
+  },
+});
